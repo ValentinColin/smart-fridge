6445ce18d28de7f75928eff3d39538f423a573df
Valentin Colin <valentin.colin78@gmail.com>
2023-09-10 22:35:21 +0200

    Modification de la BDD
    Signed-off-by: Valentin Colin <valentin.colin78@gmail.com>

--- db/migrations/2_add_food.sql
+++ db/migrations/2_add_food.sql
@@ -1,5 +1,17 @@
 -- Ajoute des valeurs initial dans la base de donnée pour l'exemple
-INSERT INTO food (name)
-SELECT name
-FROM (VALUES ('Banane'), ('Banane'), ('Yaourt'), ('Viande'), ('Tomate')) AS new_rows(name)
+INSERT INTO food (name, expiration_date)
+SELECT name, expiration_date
+FROM (VALUES
+          ('Banane', CURRENT_DATE - 1),
+          ('Banane', CURRENT_DATE + 4),
+          ('Yaourt', CURRENT_DATE + 3),
+          ('Viande', CURRENT_DATE + 2),
+          ('Tomate', CURRENT_DATE + 5)
+      ) AS new_rows(name, expiration_date)
 WHERE NOT EXISTS (SELECT 1 FROM food);
+
+-- Et si il y a déjà des données qui ont une date d'expiration null,
+-- alors on set leurs date d'expiration à aujourd'hui.
+UPDATE food
+SET expiration_date = current_date
+WHERE expiration_date IS NULL;
