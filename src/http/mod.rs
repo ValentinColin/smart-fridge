df3f4702d6d6ad1d6019cd3630a66b2fe529ce61
Valentin Colin <valentin.colin78@gmail.com>
2023-09-13 19:40:09 +0200

    Move tests
    Signed-off-by: Valentin Colin <valentin.colin78@gmail.com>

--- src/http/mod.rs
+++ src/http/mod.rs
@@ -26,3 +26,60 @@ pub fn app(db: PgPool) -> Router {
         .route("/delete/:id", get(handler::delete_food))
         .layer(Extension(db))
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::db::get_db_url;
+    use axum::body::Body;
+    use axum::http::{Request, StatusCode};
+    use sqlx::postgres::PgPoolOptions;
+    use tower::util::ServiceExt;
+
+    #[tokio::test]
+    async fn axum_healthcheck() {
+        let db = PgPoolOptions::new()
+            .max_connections(5)
+            .connect(&get_db_url().unwrap())
+            .await
+            .unwrap();
+        let app = app(db);
+
+        // `Router` implements `tower::Service<Request<Body>>` so we can
+        // call it like any tower service, no need to run an HTTP server.
+        let response = app
+            .oneshot(Request::builder().uri("/test").body(Body::empty()).unwrap())
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::OK);
+
+        let body = hyper::body::to_bytes(response.into_body()).await.unwrap();
+        assert_eq!(&body[..], b"OK");
+    }
+
+    #[tokio::test]
+    async fn axum_not_found() {
+        let db = PgPoolOptions::new()
+            .max_connections(5)
+            .connect(&get_db_url().unwrap())
+            .await
+            .unwrap();
+        let app = app(db);
+
+        let response = app
+            .oneshot(
+                Request::builder()
+                    .uri("/does-not-exist")
+                    .body(Body::empty())
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::NOT_FOUND);
+
+        let body = hyper::body::to_bytes(response.into_body()).await.unwrap();
+        assert!(body.is_empty());
+    }
+}
